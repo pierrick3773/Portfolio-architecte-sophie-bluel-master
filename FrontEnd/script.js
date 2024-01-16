f9dd8037fd4b8e77b7f67dbacbe2a2b11c1f29f6
pierrick3773 <pviarde@gmail.com>
2024-01-16 11:57:12 +0100

    fonction affichage d'image

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -5,24 +5,22 @@ const API_BASE_URL = "http://localhost:5678/api"
 function getWorks() {
   return fetch (`${API_BASE_URL}/works`)
   .then (Response => Response.json () )
-  .then ( data => { console.log(data);})
-  .then(data => {
-    // appeler une function de display images
-    displayImages(data);
-  })
+  .then ( data => { console.log(data);
+  return data})
+  
   
     
   } 
   function displayImages(data) {
     
     const imageContainer = document.getElementById('figure');
-    data.forEach(data => {
+    for (const work of data) {
       const imgElement = document.createElement('img');
       imgElement.src = work.imageUrl;
       imgElement.alt = work.title;
 
-      imageContainer.appendChild(imgElement);
-    });
+      imageContainer.appendChild(img);
+    };
   }
       document.addEventListener('DOMContentLoaded', () => {
         getWorks();
