66eb5bc62f4c655d88388f376b15106f4ea79b92
pierrick3773 <pviarde@gmail.com>
2024-01-16 15:23:04 +0100

    ajout des imgCapation

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,37 +1,36 @@
 console.log("hello world");
 
-const API_BASE_URL = "http://localhost:5678/api"
+const API_BASE_URL = "http://localhost:5678/api";
 
 function getWorks() {
-  return fetch (`${API_BASE_URL}/works`)
-  .then (Response => Response.json () )
-  .then ( data => { console.log(data);
-  return data})
-  .then(data => {
-    // appeler une function de display images
-    displayImages(data);
-  })
-  
-    
-  } 
-  function displayImages(data) {
-    
-    const imageContainer = document.getElementsByClassName(`gallery`)[0]
+  return fetch(`${API_BASE_URL}/works`)
+    .then((Response) => Response.json())
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .then((data) => {
+      // appeler une function de display images
+      displayImages(data);
+    });
+}
+function displayImages(data) {
+  const imageContainer = document.querySelector(`.gallery`);
+  console.log(imageContainer);
+  for (const works of data) {
+    const imgDiv = document.createElement("figure");
+    const imgElement = document.createElement("img");
+    const imgCap = document.createElement(`figcaption`);
+    imgElement.src = works.imageUrl;
+    imgElement.alt = works.title;
+    imgCap.textContent = works.title;
+    console.log(`figcaption`);
     console.log(imageContainer);
-    for (const works of data) {
-      const imgDiv = document.createElement('figure');
-      const imgElement = document.createElement('img');
-      imgElement.src = works.imageUrl;
-      imgElement.alt = works.title;
-      console.log(imageContainer);
-      imageContainer.appendChild(imgDiv);
-      imgDiv.appendChild(imgElement)
-    };
+    imageContainer.appendChild(imgDiv);
+    imgDiv.appendChild(imgElement);
+    imgDiv.appendChild(imgCap);
   }
-      document.addEventListener('DOMContentLoaded', () => {
-        getWorks();
-      });
-
-
-
-
+}
+document.addEventListener("DOMContentLoaded", () => {
+  getWorks();
+});
