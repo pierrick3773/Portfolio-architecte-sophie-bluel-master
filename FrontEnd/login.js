7f248a274e7e09e23e19bd7c9508f712d2a43b54
pierrick3773 <pviarde@gmail.com>
2024-01-24 17:02:32 +0100

    fetch login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,50 +1,32 @@
 console.log("hello world");
 const API_BASE_URL = "http://localhost:5678/api";
+console.log(API_BASE_URL);
 
 const buttonConnecter = document.querySelector(`.connect`);
 console.log(buttonConnecter);
-
-// // // // function connect(buttonConnecter, works, categories) {
-// // // // buttonConnecter.addEventListener(`click`, () => {
-// // // const connect = fetch(`${API_BASE_URL}/users/login`, { method: "post" }).then(
-// // //   (response) => {
-// // //     //handle response
-// // //     console.log(response);
-// // //   }
-// // // );
-// // // // .then((Response) => Response.json())
-// // // // .then((data) => {
-// // // //   console.log(data);
-// // // //   return data;
-// // // //   .then token === "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4"
-// // // // });
-// // // //  });
-// // // // }
-// buttonConnecter.addEventListener(`click`, () => {
-
-//   .then((response) => response.json())
-//   .then((data) => console.log(data))
-//   .catch((error) => console.error("Error:", error));
-// // });
 
 function logIn() {
   const mail = document.getElementById(`email`).value;
   const password = document.getElementById(`password`).value;
   console.log(mail);
   console.log(password);
+  return {
+    email: mail,
+    password: password,
+  };
 }
-logIn();
-let data = {
-  email: `mail`,
-  password: `password`,
-};
+const data = logIn();
+
 console.log(data);
 
-function logToken(data) {
-  fetch(`${API_BASE_URL}/users/login`, {
+async function logToken(data) {
+  const result = await fetch(`${API_BASE_URL}/users/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
-  });
+  }).then((res) => res.json());
+  console.log(result);
 }
-logToken();
+buttonConnecter.addEventListener(`click`, () => {
+  logToken(data);
+});
